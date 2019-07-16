e41e711e77550f1157b40328cafc9ecd3f08ab68
ParkerGits <parkerjlandon@gmail.com>
2019-07-16 15:16:40 -0700

    plunkr to desktop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import {Component} from "@angular/core";
-import { trigger, state, style} from "@angular/animations";
+import { trigger, state, style, transition, animate} from "@angular/animations";
+
 @Component({
     selector: 'app',
     animations:[
         trigger('signal', [
             state('go', style({
-                'background-color':'green'
-            }))
+                'background-color':'green',
+                'height':'100px'
+            })),
+            state('stop', style({
+                'background-color':'red',
+                'height':'50px'
+            })),
+            transition('* => *', animate(500))
         ])
     ],
     styles:[`
@@ -18,11 +25,23 @@ import { trigger, state, style} from "@angular/animations";
 `],
     template: `
 <div
-    [@signal]="'go'"
-    class="traffic-light">
-    
+    [@signal]="signal"
+    class="traffic-light"
+    >
+
 </div>
+<button (click)="onGoClick()">Go</button>
+<button (click)="onStopClick()">Stop</button>
 `
 })
 export class AppComponent {
+    signal;
+
+    onGoClick(){
+        this.signal = 'go';
+    }
+
+    onStopClick(){
+        this.signal = 'stop';
+    }
 }
