a979fc9ed3cf7d3ffd5c4b7603bb97ca57188640
Creeland <cree@provinsal.com>
2019-07-15 17:22:03 -0700

    Add lesson-01 files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from "@angular/platform-browser";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from "./app.component";
+import {NgModule} from "@angular/core";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports:[
+      BrowserModule,
+      BrowserAnimationsModule],
+    declarations:[AppComponent],
+    bootstrap:[AppComponent]
 })
-export class AppModule { }
+export class AppModule{}
