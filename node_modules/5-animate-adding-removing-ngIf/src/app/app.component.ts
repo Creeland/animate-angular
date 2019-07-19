1e11a6a48b850aff65198c9ac458e96e81fe84d4
Parker <parkerjlandon@gmail.com>
2019-07-19 00:36:25 -0700

    yarn workspace

---
+++ node_modules/5-animate-adding-removing-ngIf/src/app/app.component.ts
@@ -0,0 +1,58 @@
+import {Component} from "@angular/core";
+import { trigger, state, style, transition, animate} from "@angular/animations";
+
+@Component({
+    selector: 'app',
+    animations:[
+        trigger('signal', [
+            state('void', style({
+                'transform':'translateY(-100%)'
+            })),
+            state('go', style({
+                'background-color':'green',
+                'height':'100px'
+            })),
+            state('stop', style({
+                'background-color':'red',
+                'height':'50px'
+            })),
+            transition('* => *', animate(500))
+        ])
+    ],
+    styles:[`
+.traffic-light{
+    width: 100px;
+    height: 100px;
+    background-color: black;
+}
+`],
+    template: `
+<div
+    [@signal]="signal"
+    class="traffic-light"
+    *ngIf="isHere"
+    >
+
+</div>
+<button (click)="onGoClick()">Go</button>
+<button (click)="onStopClick()">Stop</button>
+<hr>
+<button (click)="onToggleClick()">Toggle</button>
+`
+})
+export class AppComponent {
+    signal;
+    isHere = false;
+
+    onGoClick(){
+        this.signal = 'go';
+    }
+
+    onStopClick(){
+        this.signal = 'stop';
+    }
+
+    onToggleClick(){
+        this.isHere = !this.isHere;
+    }
+}
